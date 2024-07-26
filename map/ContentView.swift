2c66e187d10ab9d1cbee483fe4c57d80aeacb061
Han Khang <s3986602@rmit.edu.vn>
2024-07-26 08:55:00 +0700

    Initial Commit

---
+++ map/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  map
+//
+//  Created by khang on 26/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ map/mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapApp.swift
+//  map
+//
+//  Created by khang on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
